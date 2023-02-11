f5d59017d24a6549b13ef463c34df16e6cc9e48b
Boz-Yusuf <boz35yusuf@gmail.com>
2023-02-11 18:14:46 +0300

    refactor

--- main.js
+++ main.js
@@ -12,44 +12,43 @@ submitButton.addEventListener('click',Register);
 
 
 function Register(){
-    LengthValidation(username,"username");
-    LengthValidation(password,"password");
+    LengthValidation(username);
+    LengthValidation(password);
     SamePasswordValidation(password,password2);
     MailValidation(email);
 }
 
-function LengthValidation(value,valueType){
+function LengthValidation(value){
     if(value.value.length < 6){  
-        value.parentElement.children[2].innerText = `${valueType} must be at least 6 characters`;
         value.style.border = "2px solid var(--waring-color)";
+        value.parentElement.children[2].style.visibility = "visible"
     }else{
-        value.parentElement.children[2].innerText = ``;
-        value.parentElement.children[2].append(emptyRow);
         value.style.border = "2px solid var(--text-color)";
+        value.parentElement.children[2].style.visibility = "hidden"
     }
 }
 
 function MailValidation(email){
     if(!email.value.match(pattern)){
-        email.parentElement.children[2].innerText = `Email is not valid`;
+        email.parentElement.children[2].style.visibility = "visible";
         email.style.border = "2px solid var(--waring-color)";
     }else{
+        email.parentElement.children[2].style.visibility = "hidden";
         email.style.border = "2px solid var(--text-color)";
-        email.parentElement.children[2].innerText = ``;
-        email.parentElement.children[2].append(emptyRow);
     }
 }
 
 function SamePasswordValidation(password,password2){
     if(password2.value === ""){
-        password2.parentElement.children[2].innerText = `Confirm Password is required`;
+        password2.parentElement.children[2].style.visibility = "visible";
         password2.style.border = "2px solid var(--waring-color)";
     }else if(password.value !== password2.value){
         password2.parentElement.children[2].innerText = `Passwords do not match`;
+        password2.parentElement.children[2].style.visibility = "visible";
         password2.style.border = "2px solid var(--waring-color)";
     }else{
         password2.style.border = "2px solid var(--text-color)";
-        password2.parentElement.children[2].innerText = ``;
+        password2.parentElement.children[2].style.visibility = "hidden";
         password2.parentElement.children[2].append(emptyRow);
     }
 }
